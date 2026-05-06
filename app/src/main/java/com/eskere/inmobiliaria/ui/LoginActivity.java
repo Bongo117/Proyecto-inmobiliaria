62af60f6cf8efa138dcc537772523db144320e10
Bongo117 <luchosoto117@gmail.com>
2026-05-06 20:35:31 -0300

    Update LoginActivity.java

--- app/src/main/java/com/eskere/inmobiliaria/ui/LoginActivity.java
+++ app/src/main/java/com/eskere/inmobiliaria/ui/LoginActivity.java
@@ -2,8 +2,6 @@ package com.eskere.inmobiliaria.ui;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
-// 1. Importar la clase de ViewBinding autogenerada
 import com.eskere.inmobiliaria.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
